43c5cb2e5a21baf29aee8e15527fc9c5197238d5
cgroll <groll.christian.edu@gmail.com>
2015-01-14 19:15:07 +0100

    pdf, rand for ParamPC and ModPC

--- src/pdf.jl
+++ src/pdf.jl
@@ -1,9 +1,59 @@
+## parametric copula pdf
+##----------------------
+
 function pdf(cop::ParamPC, u::Float64, v::Float64)
-    ## get family and parameters
-    fam = float(getCopId(cop))
-    params = cop.params
+    fam, params = getFamAndParams(cop)
 
     @mput fam u v params
     val = @matlab PairCopulaPDF(fam, u, v, params)
+    return val
+end
+
+## vector inputs
+function pdf(cop::ParamPC, u::Array{Float64, 1},
+             v::Array{Float64, 1})
+    checkSameLength(u, v)
+        
+    fam, params = getFamAndParams(cop)
+
+    @mput fam u v params
+    val = @matlab PairCopulaPDF(fam, u, v, params)
+    return val
+end
+
+## parametric copula pdf with rotation
+##------------------------------------
+
+function pdf(cop::ModPC, u::Float64, v::Float64)
+    return pdf(cop.cop, cop.mod, u, v)
+end
+
+function pdf(cop::ParamPC, rot::Rotation, u::Float64, v::Float64)
+    fam, params = getFamAndParams(cop)
+
+    degr = rot.degrees
+
+    @mput fam u v params degr
+    val = @matlab PairCopulaPDF(fam, u, v, params, degr)
+    return val
+end
+
+## vector inputs
+function pdf(cop::ModPC, u::Array{Float64, 1},
+             v::Array{Float64, 1})
+    return pdf(cop.cop, cop.mod, u, v)
+end
+
+function pdf(cop::ParamPC, rot::Rotation,
+             u::Array{Float64, 1},
+             v::Array{Float64, 1})
+    checkSameLength(u, v)
+    
+    fam, params = getFamAndParams(cop)
+
+    degr = rot.degrees
+
+    @mput fam u v params degr
+    val = @matlab PairCopulaPDF(fam, u, v, params, degr)
     return val
 end
