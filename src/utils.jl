bff2de777d04c982caa934186758c7c6fc42af3b
cgroll <groll.christian.edu@gmail.com>
2015-01-15 21:55:35 +0100

    allow different handling of parameters in future

--- src/utils.jl
+++ src/utils.jl
@@ -6,5 +6,16 @@ function checkSameLength(u::Array{Float64, 1}, v::Array{Float64, 1})
 end
 
 function getFamAndParams(cop::ParamPC)
-    return (float(getCopId(cop)), cop.params)
+    return (float(getCopId(cop)), paramsMatlab(cop))
+end
+
+## this functions exists so that copula types at some point can be
+## changed to follow the interface of Distributions.jl more closely.
+## This means that each copula type will have multiple fields, one for
+## each parameter, and function params(cop::ParamCop) will generally
+## return a tuple. This allows additional flexibility, since a single
+## parameter then can be more than just a single Float value, but it
+## could also be a covariance matrix or a PCModification.
+function paramsMatlab(cop::PairCop)
+    return cop.params
 end
