9ec618c4efd06d951daaf656ec5dabb4ccfe8f14
cgroll <groll.christian.edu@gmail.com>
2015-02-06 14:14:49 +0100

    different way to handle Requires.jl

--- Makefile
+++ Makefile
@@ -1,7 +1,6 @@
 LIBRARY_DIR := src/VineCopulaCPP
 
 all:
-	julia -e 'try Pkg.installed("Requires"); catch; Pkg.clone("https://github.com/one-more-minute/Requires.jl.git") end'
 	git submodule init
 	git submodule update
 	make -C $(LIBRARY_DIR)
--- deps/build.jl
+++ deps/build.jl
@@ -1,2 +1,11 @@
+## install Requires.jl
+try
+    if isa(Pkg.installed("Requires"), Nothing)
+        Pkg.clone("https://github.com/one-more-minute/Requires.jl.git")
+    end
+catch
+    Pkg.clone("https://github.com/one-more-minute/Requires.jl.git")
+end
+
 cd(Pkg.dir("Copulas"))
 run(`make`)
