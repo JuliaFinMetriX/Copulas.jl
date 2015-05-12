2d9e6331f4c84fe5f7ea52a7045f2c436f46b242
cgroll <groll.christian.edu@gmail.com>
2015-05-12 18:14:30 +0200

    graphviz first restructuring

--- src/PCC/Vines/graphviz.jl
+++ src/PCC/Vines/graphviz.jl
@@ -1,3 +1,53 @@
+
+## for each possible vizualization, there exist three different output
+## ways:
+## - print dot code to Julia console
+## - visualize dot code through x11
+## - write visualization of dot code to output file
+##
+## all three output ways make use of the same core: Julia code that
+## automatically creates dot code
+
+function toConsole(obj::Any; chrt="default", args...)
+    stdin = IOBuffer()
+    toGviz(obj, stdin, chrt; args...)
+    takebuf_string(stdin)
+end
+
+@doc doc"""
+Plot tree in x11 window.
+"""->
+function viz(obj::Any; cmd="dot", chrt="default", args...)
+    stdin, proc = open(`$cmd -Tx11`, "w")
+    toGviz(obj, stdin, chrt; args...)
+    close(stdin)
+end
+
+@doc doc"""
+Render chart to output file.
+"""->
+function render(obj::Any,
+                fmt::String,
+                fname::String;
+                cmd="dot",
+                chrt="default",
+                args...)
+    stdin, proc = open(`$cmd -T$fmt -o $fname.$fmt`, "w")
+    toGviz(obj, stdin, chrt; args...)
+    close(stdin)
+end
+
+@doc doc"""
+Render chart to default output file.
+"""->
+function render(obj::Any; chrt="default", cmd="dot", args...)
+    ## create tmp file
+    fname = tempname()
+    render(obj, "svg", fname; chrt=chrt, cmd=cmd, args...)
+    return string(fname, ".", "svg")
+end
+
+
 ###############
 ## tree plot ##
 ###############
@@ -7,66 +57,75 @@ Core of the graphviz interface: this function determines how a CTree
 from Julia is actually translated into graphviz code. To keep it
 general, this function will write to a given stream. This way one can
 easily replace the stream with for example a file or a process.
+
+Chrt input is irrelevant, as long as there is only one kind of
+visualization for this data type.
 """->
-function toGviz(tr::CTreeParRef, stream::IO)
+function toGviz(tr::CTreeParRef,
+                stream::IO,
+                chrt::String;
+                shape="circle",
+                arrowhead="none",
+                emph1=[], emphFillColor1="lawngreen",
+                emphShape1="circle",
+                emph2=[], emphFillColor2="lawngreen",
+                emphShape2="circle"
+                )
+    
+    ## emph1 and emph2 sets need to be non-overlapping
+    if !isempty(intersect(emph1, emph2))
+        error("emphasized sets need to be non-overlapping")
+    end
+
     nVars = size(tr.tree, 1)
     write(stream, "digraph {\n")
-    write(stream, "node [shape=circle];\n")
+    write(stream, "node [shape=$shape];\n")
+    write(stream, "edge [arrowhead=$arrowhead];\n")
     for ii=1:nVars
         parNode = tr.tree[ii]
         if parNode != 0
-            write(stream, "$parNode -> $ii;\n")
+            if ii in emph1
+                write(stream, "$ii [style=filled; fillcolor=$emphFillColor1; shape=$emphShape1];")
+                
+            elseif ii in emph2
+                write(stream, "$ii [style=filled; fillcolor=$emphFillColor2; shape=$emphShape2];")
+            else
+                
+                end
+            write(stream, "$parNode -> $ii ;\n")
         end
     end
     write(stream, "}")
 end
 
-function toGviz(tr::CTreeParRef)
-    str = IOBuffer()
-    toGviz(tr, str)
-    takebuf_string(str)
-end
-
-function toGviz(tr::AbstractCTree)
+function toGviz(tr::AbstractCTree, stream::IO; args...)
     trPar = convert(CTreeParRef, tr)
-    return toGviz(trPar)
-end
-
-@doc doc"""
-Plot tree in x11 window.
-"""->
-function viz(tr::CTreeParRef; cmd="dot")
-    stdin, proc = open(`$cmd -Tx11`, "w")
-    toGviz(tr, stdin)
-    close(stdin)
-end
-
-function viz(tr::AbstractCTree; cmd="dot")
-    trPar = convert(CTreeParRef, tr)
-    viz(trPar, cmd = cmd)
-end
-
-function render(tr::CTreeParRef,
-                fmt::String,
-                cmd::String,
-                fname::String)
-    stdin, proc = open(`$cmd -T$fmt -o $fname.$fmt`, "w")
-    toGviz(tr, stdin)
-    close(stdin)
-end
-
-function render(tr::CTreeParRef)
-    ## create tmp file
-    fname = tempname()
-    render(tr, "svg", fname)
-    return string(fname, ".", "svg")
+    return toGviz(trPar, stream; args...)
 end
 
 ###############
 ## vine plot ##
 ###############
 
-function toGviz(vn::Copulas.Vine, stream::IO, maxLayer::Int)
+## header function to delegate to distinct vine visualization
+## functions 
+function toGviz(vn::Copulas.Vine,
+                stream::IO,
+                chrt::String;
+                args...)
+    if chrt == "default"
+        toGviz_graph(vn, stream; args...)
+    else
+        toGviz_trees(vn, stream; args...)
+    end
+end
+
+## graph visualization
+##--------------------
+
+function toGviz_graph(vn::Copulas.Vine,
+                      stream::IO;
+                      maxLayer=2)
     nVars = size(vn.trees, 1)
     layers = Copulas.linkLayers(vn)
     write(stream, "graph G {\n")
@@ -88,34 +147,47 @@ function toGviz(vn::Copulas.Vine, stream::IO, maxLayer::Int)
     write(stream, "}")
 end
 
-function toGviz(vn::Copulas.Vine, maxLayer::Int)
-    str = IOBuffer()
-    toGviz(vn, str, maxLayer)
-    takebuf_string(str)
-end
 
+## conditioning tree visualization
+##--------------------------------
 
-function viz(vn::Copulas.Vine, maxLayer::Int; cmd="neato")
-    stdin, proc = open(`$cmd -Tx11`, "w")
-    toGviz(vn, stdin, maxLayer)
-    close(stdin)
-end
-
-#########################################
-## Vine as array of conditioning trees ##
-#########################################
-
-function toGvizTree(vn::Copulas.Vine, stream::IO)
+function toGviz_trees(vn::Copulas.Vine,
+                      stream::IO;
+                      shape="circle",
+                      arrowhead="none",
+                      emph1=[], emphFillColor1="lawngreen",
+                      emphFontColor1="black",
+                      emphShape1="circle",
+                      emph2=[], emphFillColor2="lawngreen",
+                      emphShape2="circle")
     nVars = size(vn.trees, 1)
     write(stream, "digraph {\n")
-    write(stream, "node [shape=circle];\n")
-    write(stream, "edge [arrowhead=none];\n")
+    write(stream, "node [shape=$shape];\n")
+    write(stream, "edge [arrowhead=$arrowhead];\n")
     for rootVar=1:nVars
-        write(stream, "\"$rootVar in $rootVar\" [label=\"$rootVar\"];\n")
+        if rootVar in emph1
+            write(stream, "\"$rootVar in $rootVar\"
+[label=\"$rootVar\"; color=$emphFillColor1; fontcolor=$emphFontColor1; style=filled; fillcolor=$emphFillColor1; shape=$emphShape1];\n")
+        elseif rootVar in emph2
+            write(stream, "\"$rootVar in $rootVar\"
+[label=\"$rootVar\"; color=$emphFillColor2; fontcolor=$emphFontColor2;
+style=filled; fillcolor=$emphFillColor2; shape=$emphShape2];\n")
+        else
+            write(stream, "\"$rootVar in $rootVar\" [label=\"$rootVar\"];\n")
+        end
         for ii=1:nVars
             parNode = vn.trees[ii, rootVar]
             if parNode > 0
-                write(stream, "\"$ii in $rootVar\" [label=\"$ii\"];\n")
+                if ii in emph1
+                    write(stream, "\"$ii in $rootVar\"
+[label=\"$ii\"; color=$emphFillColor1; fontcolor=$emphFontColor1; style=filled; fillcolor=$emphFillColor1; shape=$emphShape1];\n")
+                elseif ii in emph2
+                    write(stream, "\"$ii in $rootVar\"
+[label=\"$ii\"; color=$emphFillColor2; fontcolor=$emphFontColor2;
+style=filled; fillcolor=$emphFillColor2; shape=$emphShape2];\n")
+                else
+                    write(stream, "\"$ii in $rootVar\" [label=\"$ii\"];\n")
+                end
                 write(stream, "\"$parNode in $rootVar\" -> \"$ii in $rootVar\";\n")
             end
         end
@@ -123,14 +195,3 @@ function toGvizTree(vn::Copulas.Vine, stream::IO)
     write(stream, "}")
 end
 
-function toGvizTree(vn::Copulas.Vine)
-    str = IOBuffer()
-    toGvizTree(vn, str)
-    takebuf_string(str)
-end
-
-function vizTree(vn::Copulas.Vine; cmd="dot")
-    stdin, proc = open(`$cmd -Tx11`, "w")
-    toGvizTree(vn, stdin)
-    close(stdin)
-end
