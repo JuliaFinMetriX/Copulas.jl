0ea3a5d43d14fb6fbdc53906597ea57dd8226415
cgroll <groll.christian.edu@gmail.com>
2015-05-14 12:23:06 +0200

    JFinM_Charts added to build instructions

--- deps/build.jl
+++ deps/build.jl
@@ -7,5 +7,18 @@ catch
     Pkg.clone("https://github.com/one-more-minute/Requires.jl.git")
 end
 
+## install JFinM_Charts.jl
+## Pkg.installed():
+## - returns true if package is installed
+## - returns false if known package is not installed
+## - throws error if package is unknown and not installed
+try
+    if isa(Pkg.installed("JFinM_Charts"), Nothing)
+        Pkg.clone("https://github.com/JuliaFinMetriX/JFinM_Charts.git")
+    end
+catch
+    Pkg.clone("https://github.com/JuliaFinMetriX/JFinM_Charts.git")
+end
+
 cd(Pkg.dir("Copulas"))
 run(`make`)
