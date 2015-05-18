bc9a0f5c9bcdb8675fe4500e1bc7045b63a57bb3
cgroll <groll.christian.edu@gmail.com>
2015-05-18 10:22:46 +0200

    vine graph chart added

---
+++ src/Plotting/VineGraphChart.jl
@@ -0,0 +1,41 @@
+type VineGraphChart <: JFinM_Charts.AbstractChart
+    chartType::String
+    cmd::String
+    shape::String
+    maxLayer::Int
+    styles::Array{String, 1}
+    colors::Array{String, 1}
+    lens::Array{String, 1}
+    weights::Array{String, 1}
+end
+
+VineGraphChart() = VineGraphChart("VineGraphChart",
+                                  "neato",
+                                  "circle",
+                                  2,
+                                  ["bold", "dotted"],
+                                  ["black", "black"],
+                                  ["1.0", "1.8"],
+                                  ["1", "1"]
+                                  )
+
+
+function GViz(vn::Copulas.Vine,
+              chrt::VineGraphChart)
+    nVars = size(vn.trees, 1)
+    layers = Copulas.linkLayers(vn)
+    dotCode = ""
+    dotCode = string(dotCode, "graph G {\n")
+    dotCode = string(dotCode, "node [shape=$(chrt.shape)];\n")
+    for ii=1:nVars
+        for jj=ii+1:nVars
+            currLayer = layers[ii, jj]
+            if currLayer <= chrt.maxLayer
+                tmpCode = "$ii -- $jj [style=$(chrt.styles[currLayer]); color=$(chrt.colors[currLayer]); len=$(chrt.lens[currLayer]); weight=$(chrt.weights[currLayer])];\n"
+                    dotCode = string(dotCode, tmpCode)
+            end
+        end
+    end
+    dotCode = string(dotCode, "}")
+    return GViz("VineGraphChart", "neato", dotCode)
+end
--- test/graphviz_test.jl
+++ test/graphviz_test.jl
@@ -41,6 +41,36 @@ vn = Copulas.testvine(3)
 Copulas.viz(vn, JFinM_Charts.chart(Copulas.VineTreesChart,
                                    emph1 = [1, 3]))
 
+
+####################
+## VineGraphChart ##
+####################
+
+vn = Copulas.testvine(3)
+Copulas.viz(vn, JFinM_Charts.chart(Copulas.VineGraphChart))
+
+
+chrt = JFinM_Charts.chart(Copulas.VineGraphChart,
+                          maxLayer=3,
+                          styles=["bold", "filled", "dotted"],
+                          colors=["red", "black", "black"],
+                          lens=["1.0", "3.0", "5.0"],
+                          weights=["1", "1", "1"])
+
+Copulas.viz(vn, chrt)
+
+
+chrt = JFinM_Charts.chart(Copulas.VineGraphChart,
+                          maxLayer=2,
+                          styles=["bold", "dotted"],
+                          colors=["black", "black"],
+                          lens=["1.0", "1.8"],
+                          weights=["1", "0"])
+Copulas.viz(vn, chrt)
+
+vn = Copulas.testvine(8)
+Copulas.viz(vn, Copulas.VineGraphChart())
+
 ## Copulas.viz(tr)
 
 ## expGraphvizString = """
